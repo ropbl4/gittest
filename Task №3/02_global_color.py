c870b2f2174af6db72b420b54ecf3d96d379aa36
Alex <ivankiv-alex@mail.ru>
2020-10-22 21:51:47 +0300

    Tasks 2-3 done.

--- Task №3/02_global_color.py
+++ Task №3/02_global_color.py
@@ -15,5 +15,31 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
 # TODO здесь ваш код
+sd.resolution = (1600, 1000)
+COLORS = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+
+def parallelogramm(point, count_of_angles, start_angle=0, length=200, color=COLORS[2]):
+    angle_between_lines = 360 / count_of_angles
+    angle_sum = start_angle
+
+    for _ in range(count_of_angles):
+        point = parallelogramm_draw(point, angle_sum, length, color=color)
+        angle_sum += angle_between_lines
+
+
+def parallelogramm_draw(point, angle=0, length=200, color=COLORS[2]):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+    return v1.end_point
+
+
+point_0 = sd.get_point(900, 500)
+count_of_angles = 6
+angle = 0
+color = int(input(f'Введите число 0-{len(COLORS)-1} (это выбор цвета): '))
+
+for angle in range(0, 361, 360 // count_of_angles):
+    parallelogramm(point=point_0, count_of_angles=count_of_angles, start_angle=angle, length=100, color=COLORS[color])
 
 sd.pause()
--- Task №3/03_shape_select.py
+++ Task №3/03_shape_select.py
@@ -10,5 +10,37 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
 # TODO здесь ваш код
+sd.resolution = (1600, 1000)
+COLORS = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+FIGURES = {3: 'Треугольник', 4: 'Квадрат', 5: '5-угольник', 6: '6-угольник'}
+
+def parallelogramm(point, count_of_angles, start_angle=0, length=200, color=COLORS[2]):
+    angle_between_lines = 360 / count_of_angles
+    angle_sum = start_angle
+
+    for _ in range(count_of_angles):
+        point = parallelogramm_draw(point, angle_sum, length, color=color)
+        angle_sum += angle_between_lines
+
+
+def parallelogramm_draw(point, angle=0, length=200, color=COLORS[2]):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+    return v1.end_point
+
+
+point_0 = sd.get_point(900, 500)
+angle = 0
+
+for i in FIGURES:
+    print(i, '-', FIGURES[i])
+
+figure = int(input(f'Введите число из списка выше (это выбор фигуры):'))
+count_of_angles = figure
+
+color = int(input(f'Введите число 0-{len(COLORS)-1} (это выбор цвета): '))
+
+for angle in range(0, 361, 360 // count_of_angles):
+    parallelogramm(point=point_0, count_of_angles=count_of_angles, start_angle=angle, length=100, color=COLORS[color])
 
 sd.pause()
